4415e048b6b9d85ac9dd5de210faf72b017ca597
Thaison Oliveira <Thaisonoliveira@gmail.com>
2022-02-03 22:38:13 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,39 @@
+// O arquivos _app.js é responsável por controlar o estilo do site do forma global
+function GlobalStyle() {
+	return (
+		<style global jsx>{`
+		* {
+		  margin: 0;
+		  padding: 0;
+		  box-sizing: border-box;
+		  list-style: none;
+		}
+		body {
+		  font-family: 'Open Sans', sans-serif;
+		}
+		/* App fit Height */ 
+		html, body, #__next {
+		  min-height: 100vh;
+		  display: flex;
+		  flex: 1;
+		}
+		#__next {
+		  flex: 1;
+		}
+		#__next > * {
+		  flex: 1;
+		}
+		/* ./App fit Height */ 
+	  `}</style>
+	);
+}
+
+export default function MyApp({ Component, pageProps }) {
+  console.log('Roda em todas as páginas!');
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );  
+}
